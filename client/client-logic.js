8f087c15906ed35aab3e5775391e8703d538eeec
kfwong <kfwong@zoho.com>
2018-10-18 15:08:49 +0800

    new organization.

--- client/client-logic.js
+++ client/client-logic.js
@@ -3,11 +3,35 @@ let mainMenu = document.getElementById('main-menu');
 let joinRoom = document.getElementById('join-room');
 let inGame = document.getElementById('in-game');
 var client = null;
+var playerName = "";
+var roomId = "";
 
 var gestureListenerIntervals = {};
 var gestureCacheStore = {};
 var sensor = null;
 
+
+var processShake = setInterval(() => {
+    let accVal = gestureCacheStore['shake'];
+    let squaredLength = accVal.x * accVal.x + accVal.y * accVal.y + accVal.z * accVal.z;
+    console.log("shake saving", accVal, squaredLength);
+    if (squaredLength >= 25) {
+        console.log("past threshold, sending values");
+        this.client.player.send(JSON.stringify({
+            type: "shake",
+            user: playerName,
+            payload: "shakeSent"
+        }));
+    }
+    // Unity source for reference
+    //if (Input.acceleration.sqrMagnitude >= _sqrShakeDetectionThreshold
+    //    && Time.unscaledTime >= _timeSinceLastShake + _minShakeInterval)
+    //{
+    //    _rb2d.AddForce(new Vector2(0, 1000));
+    //    _timeSinceLastShake = Time.unscaledTime;
+    //    _animator.SetTrigger("Flap");
+    //}
+}, 200);
 
 // client gesture handlers
 var startShakeDetect = () => {
@@ -28,27 +52,7 @@ var startShakeDetect = () => {
         }, false);
     }
 
-    gestureListenerIntervals['shake'] = setInterval(() => {
-        let accVal = clientObj.gestureCacheStore['shake'];
-        let squaredLength = accVal.x * accVal.x + accVal.y * accVal.y + accVal.z * accVal.z;
-        console.log("shake saving", accVal, squaredLength);
-        if (squaredLength >= 25) {
-            console.log("past threshold, sending values");
-            client.player.send(JSON.stringify({
-                type: "shake",
-                user: clientObj.playerName,
-                payload: "shakeSent"
-            }));
-        }
-        // Unity source for reference
-        //if (Input.acceleration.sqrMagnitude >= _sqrShakeDetectionThreshold
-        //    && Time.unscaledTime >= _timeSinceLastShake + _minShakeInterval)
-        //{
-        //    _rb2d.AddForce(new Vector2(0, 1000));
-        //    _timeSinceLastShake = Time.unscaledTime;
-        //    _animator.SetTrigger("Flap");
-        //}
-    }, 200);
+    gestureListenerIntervals['shake'] = processShake;
 }
 
 var stopShakeDetect = () => {
@@ -77,11 +81,13 @@ function joinRoomPlayClicked() {
     joinRoom.style.display = "none";
     inGame.style.display = "block";
 
-    var playerName = document.getElementById('usercode-input').value;
-    var roomId = document.getElementById('roomcode-input').value;
+    playerName = document.getElementById('usercode-input').value;
+    roomId = document.getElementById('roomcode-input').value;
+
     client = new SumoClient(playerName, roomId);
     client.start();
-    client.startShakeDetect();
+
+    startShakeDetect();
 
 }
 document.getElementById('join-room-join').onclick = joinRoomPlayClicked;
