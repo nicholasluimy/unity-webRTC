9fce81bd2c241292fcf548d8b05113fa1b997e13
nicholasluimy <nicholasluimy@gmail.com>
2018-11-02 14:01:58 +0800

    desktop-restyle main-menu

--- src/views/MainMenu.vue
+++ src/views/MainMenu.vue
@@ -1,16 +1,21 @@
 <template>
-  <div class="center-vertical no-scroll" id="main-menu">
-    <img class="header-image" src="@/assets/main-menu/header.png">
-    <img id="main-menu-play" class ="button" src="@/assets/main-menu/button_play.png" v-on:click="goToJoinRoom">
-    <img id="about" class="button" src="@/assets/main-menu/button_about.png" v-on:click="goToAbout">
-    <img id="help" class="button" src="@/assets/main-menu/button_help.png" v-on:click="goToHelp">
-    <img class="sumo" src="@/assets/main-menu/sumo.png">
+  <div id="desktop-container">
+    <img src="@/assets/main-menu/leftsumo.png" class="desktop-only leftsumo">
+    <div class="center-vertical no-scroll" id="main-menu">
+      <img class="header-image" src="@/assets/main-menu/header.png">
+      <img id="main-menu-play" class ="button" src="@/assets/main-menu/button_play.png" v-on:click="goToJoinRoom">
+      <img id="about" class="button" src="@/assets/main-menu/button_about.png" v-on:click="goToAbout">
+      <img id="help" class="button" src="@/assets/main-menu/button_help.png" v-on:click="goToHelp">
+      <img class="sumo" src="@/assets/main-menu/sumo.png">
+    </div>
+    <img src="@/assets/main-menu/rightsumo.png" class="desktop-only rightsumo">
   </div>
 </template>
 
 <style lang="scss" scoped>
 #main-menu {
   overflow-y: hidden;
+  z-index: 1;
 }
 
 .button {
@@ -31,6 +36,37 @@
   padding-top: 15%;
   padding-bottom: 10%;
 }
+
+#desktop-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  .desktop-only {
+    display: none;
+  }
+  .leftsumo {
+    margin-right: -8%;
+    margin-top: -30%;
+    max-width: 200px;
+  }
+  .rightsumo {
+    margin-left: -8%;
+    margin-top: -16%;
+    max-width: 200px;
+  }
+}
+
+
+
+@media (min-width: 768px) {
+  #desktop-container {
+    .desktop-only {
+      display: inherit;
+    }
+  }
+
+}
+
 </style>
 <script>
 export default {
